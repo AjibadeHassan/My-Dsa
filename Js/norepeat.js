1917fd90467edad0c15ce6cf79cbb3901a1a7c5a
Ajibade Hassan Ademidun <hassanajibade17@gmail.com>
2025-04-15 13:54:59 +0000

    non repeating

---
+++ Js/norepeat.js
@@ -0,0 +1,6 @@
+function firstNonRepeatingChar(str) {
+      const count = {};
+        for (let ch of str) count[ch] = (count[ch] || 0) + 1;
+          for (let ch of str) if (count[ch] === 1) return ch;
+            return null;
+            }
