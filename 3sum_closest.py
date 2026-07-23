8b00134f4ea6ff1fdd31b630d696a1f680710f38
Ajibade Hassan Ademidun <hassanajibade17@gmail.com>
2026-07-23 23:44:09 +0100

    Create 3sum_closest.py

---
+++ 3sum_closest.py
@@ -0,0 +1,28 @@
+class Solution(object):
+    def threeSumClosest(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        nums.sort()
+        closest_sum = float('inf')
+        min_diff = float('inf')
+
+        for i in range(len(nums) - 2):
+            left, right = i + 1, len(nums) - 1
+
+            while left < right:
+                current_sum = nums[i] + nums[left] + nums[right]
+                current_diff = abs(current_sum - target)
+
+                if current_diff < min_diff:
+                    min_diff = current_diff
+                    closest_sum = current_sum
+
+                if current_sum < target:
+                    left += 1
+                else:
+                    right -= 1
+
+        return closest_sum
